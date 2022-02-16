07212bcfec22d20148509fcc8269b662b459cdb2
musty1406 <musty1406@gmail.com>
2022-02-16 17:07:20 +0300

    rabbit mq bağlantısı kuruldu

---
+++ user-service/src/main/java/com/bilgeadam/controller/PostController.java
@@ -0,0 +1,12 @@
+package com.bilgeadam.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.bilgeadam.constant.RestApiUrls.*;
+
+
+@RestController
+@RequestMapping(VERSION+POST)
+public class PostController {
+}
